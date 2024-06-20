22d79389788e3d451434fcf978254a8faf421cf1
jhua <jhua@jhuas-MacBook-Pro.local>
2024-06-19 21:37:43 -0700

    finish v2 with multiple titles

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Deployed using https://vercel.com/guides/using-express-with-vercel
+
+Commands
+
+- vercel dev # run dev version
+- vercel # deploy to dev
+- vercel --prod # deploy to prod
--- api/index.ts
+++ api/index.ts
@@ -2,49 +2,92 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
-
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
 const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
+const DB_PREFIX = "skills_li_";
+
+interface Skill {
+  skill: string;
+  count: number;
+}
 
 const run = async () => {
   try {
     await client.connect();
     await client.db("indem").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("successfully pinged db");
   } catch (error) {
     console.log(error);
   }
 };
-
 run();
 
-app.get("/", (_, res) => res.send("((( 6('-')9"));
+app.get("/", (req: any, res: any) => res.send("((( 6('-')9"));
 
-// Get skills in descending order
-app.get("/skills", async (_, res) => {
-  let collection = await client.db("indem").collection("skills_linkedin");
-  let results = await collection
+const getSkillsForTitle = async (title: string) => {
+  const collection = await client
+    .db("indem")
+    .collection(`${DB_PREFIX}${title}`);
+  const results = await collection
     .find({})
     .project({ _id: 0, skill: 1, count: 1 })
-    .sort({ count: -1 })
     .toArray();
 
-  res.send(results).status(200);
+  return results as Array<Skill>;
+};
+
+// Get skills in descending order
+app.get("/skills", async (req: any, res: any) => {
+  // replace spaces with '_'
+  const titles =
+    req.query.titles?.split(",")?.map((t: string) => t.replace(/ /g, "_")) ||
+    [];
+
+  if (titles.length === 0) {
+    res.send({ message: "No titles selected" }).status(400);
+  }
+
+  var promiseArray: Promise<Skill[]>[] = [];
+
+  for (const title of titles) {
+    promiseArray.push(getSkillsForTitle(title));
+  }
+
+  await Promise.all(promiseArray)
+    .then((collections) => {
+      const skillsMap = new Map();
+      for (const collection of collections) {
+        for (const { skill, count } of collection) {
+          if (skillsMap.has(skill)) {
+            skillsMap.set(skill, skillsMap.get(skill) + count);
+          } else {
+            skillsMap.set(skill, count);
+          }
+        }
+      }
+
+      const data = Array.from(skillsMap, ([skill, count]) => ({
+        skill,
+        count,
+      })).sort((a, b) => (a.count > b.count ? -1 : 1));
+
+      res.send(data).status(200);
+    })
+    .catch((err) => res.send(err).status(500));
 });
 
 // Get crawler logs
-app.get("/crawler", async (_, res) => {
-  let collection = await client.db("indem").collection("crawler_log");
-  let results = await collection.find({}).project({ _id: 0 }).toArray();
+app.get("/crawler", async (req: any, res: any) => {
+  const collection = await client.db("indem").collection("crawler_log");
+  const results = await collection.find({}).project({ _id: 0 }).toArray();
 
   res.send(results).status(200);
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("server started"));
 
 module.exports = app;
