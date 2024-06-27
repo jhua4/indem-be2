4d64c5412a5d5f79870584dbf84ade2f745bd7b8
jhua <jhua@jhuas-MacBook-Pro.local>
2024-06-27 14:45:14 -0700

    add nodemon script, add salaries lookup endpoints

--- api/index.ts
+++ api/index.ts
@@ -8,11 +8,17 @@ app.use(cors());
 
 const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
-const DB_PREFIX = "skills_li_";
+const SKILLS_COLLECTION_PREFIX = "skills_li_";
+const JOBS_COLLECTION_PREFIX = "jobs_li_";
 
 interface Skill {
   skill: string;
   count: number;
+}
+
+interface Job {
+  salary_min: number;
+  salary_max: number;
 }
 
 const run = async () => {
@@ -28,53 +34,29 @@ run();
 
 app.get("/", (req: any, res: any) => res.send("((( 6('-')9"));
 
-const getSkillsForTitle = async (title: string) => {
-  const collection = await client
-    .db("indem")
-    .collection(`${DB_PREFIX}${title}`);
-  const results = await collection
-    .find({})
-    .project({ _id: 0, skill: 1, count: 1 })
-    .toArray();
-
-  return results as Array<Skill>;
-};
-
 // Get skills in descending order
 app.get("/skills", async (req: any, res: any) => {
   // replace spaces with '_'
-  const titles =
-    req.query.titles?.split(",")?.map((t: string) => t.replace(/ /g, "_")) ||
-    [];
+  const title = req.query.title?.replace(/ /g, "_") || "";
 
-  if (titles.length === 0) {
-    res.send({ message: "No titles selected" }).status(400);
+  if (!title) {
+    return res.send({ message: "No title selected" }).status(400);
   }
+  const query = async (title: string) => {
+    const collection = await client
+      .db("indem")
+      .collection(`${SKILLS_COLLECTION_PREFIX}${title}`);
+    const results = await collection
+      .find({})
+      .project({ _id: 0, skill: 1, count: 1 })
+      .sort({ count: -1 })
+      .toArray();
 
-  var promiseArray: Promise<Skill[]>[] = [];
+    return results as Array<Skill>;
+  };
 
-  for (const title of titles) {
-    promiseArray.push(getSkillsForTitle(title));
-  }
-
-  await Promise.all(promiseArray)
-    .then((collections) => {
-      const skillsMap = new Map();
-      for (const collection of collections) {
-        for (const { skill, count } of collection) {
-          if (skillsMap.has(skill)) {
-            skillsMap.set(skill, skillsMap.get(skill) + count);
-          } else {
-            skillsMap.set(skill, count);
-          }
-        }
-      }
-
-      const data = Array.from(skillsMap, ([skill, count]) => ({
-        skill,
-        count,
-      })).sort((a, b) => (a.count > b.count ? -1 : 1));
-
+  await query(title)
+    .then((data) => {
       res.send(data).status(200);
     })
     .catch((err) => res.send(err).status(500));
@@ -86,6 +68,128 @@ app.get("/crawler", async (req: any, res: any) => {
   const results = await collection.find({}).project({ _id: 0 }).toArray();
 
   res.send(results).status(200);
+});
+
+app.get("/skill-options", async (req: any, res: any) => {
+  const query = async (title: string, search: string) => {
+    const collection = await client
+      .db("indem")
+      .collection(`${SKILLS_COLLECTION_PREFIX}${title}`);
+    const results = await collection
+      .find({
+        has_salary_data: true,
+        skill: {
+          $regex: search.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), // escape special chars
+          $options: "i",
+        },
+      })
+      .project({ _id: 0, skill: 1 })
+      .toArray();
+
+    return results as Array<Skill>;
+  };
+  const searchTerm = req.query.search;
+
+  await Promise.all([
+    query("frontend_engineer", searchTerm),
+    query("backend_engineer", searchTerm),
+    query("fullstack_engineer", searchTerm),
+    query("machine_learning_engineer", searchTerm),
+  ]).then((skills) => {
+    const uniqueSkills = [...new Set(skills.flat().map((s) => s.skill))];
+    return res.send(uniqueSkills).status(200);
+  });
+  res.status(500);
+});
+
+app.get("/skill-salaries", async (req: any, res: any) => {
+  const query = async (title: string, skill: string) => {
+    const collection = await client
+      .db("indem")
+      .collection(`${JOBS_COLLECTION_PREFIX}${title}`);
+    const results = await collection
+      .find({
+        salary_min: { $exists: true },
+        salary_max: { $exists: true },
+        skills: {
+          $regex: skill.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+          $options: "i",
+        },
+      })
+      .project({ _id: 0, salary_min: 1, salary_max: 1, skills: 1 })
+      .toArray();
+
+    return results as Array<Job>;
+  };
+  const skill = req.query.skill;
+
+  await Promise.all([
+    query("frontend_engineer", skill),
+    query("backend_engineer", skill),
+    query("fullstack_engineer", skill),
+    query("machine_learning_engineer", skill),
+  ]).then((jobs) => {
+    const salaries = jobs
+      .flat()
+      .map((j) => Math.floor((j.salary_min + j.salary_max) / 2))
+      .sort((a, b) => (a < b ? -1 : 1));
+
+    if (!salaries.length) return res.send({}).status(200);
+
+    const minSalary = salaries[0];
+    const maxSalary = salaries[salaries.length - 1];
+    const averageSalary = salaries.reduce(
+      (avg, value, _, { length }) => avg + value / length,
+      0
+    );
+    const medianSalary =
+      salaries.length % 2 === 0
+        ? (salaries[salaries.length / 2] + salaries[salaries.length / 2 - 1]) /
+          2
+        : salaries[Math.floor(salaries.length / 2)];
+
+    return res
+      .send({
+        minSalary,
+        maxSalary,
+        averageSalary,
+        medianSalary,
+        numJobs: salaries.length,
+      })
+      .status(200);
+  });
+  res.status(500);
+});
+
+app.get("/db-salaries-percentage", async (req: any, res: any) => {
+  const query = async (title: string) => {
+    const collection = await client
+      .db("indem")
+      .collection(`${JOBS_COLLECTION_PREFIX}${title}`);
+    const jobsWithSalaryDataCount = await collection.countDocuments({
+      salary_min: { $exists: true },
+      salary_max: { $exists: true },
+    });
+
+    const totalJobsCount = await collection.countDocuments();
+
+    return [jobsWithSalaryDataCount, totalJobsCount] as number[];
+  };
+
+  await Promise.all([
+    query("frontend_engineer"),
+    query("backend_engineer"),
+    query("fullstack_engineer"),
+    query("machine_learning_engineer"),
+  ]).then((data) => {
+    const percentage =
+      (data.reduce((partialSum, a) => partialSum + a[0], 0) /
+        data.reduce((partialSum, a) => partialSum + a[1], 0)) *
+      100;
+
+    return res.send({ percentage }).status(200);
+  });
+  res.status(500);
 });
 
 app.listen(3000, () => console.log("server started"));
